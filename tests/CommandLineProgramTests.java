7979ab3e7e5500bf4eb1800743207717f840b9df
Will Price <will.price94@gmail.com>
2014-02-20 16:00:26 +0000

    Change imports to work with $

--- tests/CommandLineProgramTests.java
+++ tests/CommandLineProgramTests.java
@@ -1,12 +1,12 @@
-import static org.junit.Assert.*;
-import junitparams.*;
-import junitparams.converters.*;
-import junitparams.mappers.*;
 
-
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static junitparams.JUnitParamsRunner.$;
+import static org.junit.Assert.*;
 
 @RunWith(JUnitParamsRunner.class)
 public class CommandLineProgramTests {
@@ -17,6 +17,7 @@ public class CommandLineProgramTests {
 		assertEquals(expectedTruthValue, new CommandLineProgram().isPart(expectedPart, new String[]{"-p" + part}));
 	}
 	
+	@SuppressWarnings("unused")
 	private Object[] parametersForIsPart() {
 		return $(
 				$(true, 1, 1),
